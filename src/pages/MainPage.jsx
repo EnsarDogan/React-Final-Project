d088b5c2c37d0b7b24c64b5b9ea0bea0fa7b9c7f
ensar dogan <doganermansur@icloud.com>
2022-05-10 23:31:31 +0200

    created MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,19 @@
-const  MainPage  = () => {
-    return <div> MainPage </div>
-}
+import { Grid } from "@mui/material";
+import { Header } from "../components";
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <Grid container spacing={3} style={{ width: "%100" }}>
+        <Grid item xs={12} md={4}>
+          List
+        </Grid>
+        <Grid item xs={12} md={8}>
+          Map
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default  MainPage ;
+export default MainPage;
