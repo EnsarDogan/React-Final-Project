dae3ad3933545f2c142db19e7f7ca55a45986999
ensar dogan <doganermansur@icloud.com>
2022-05-10 23:31:17 +0200

    created header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { AppBar, Box, Toolbar, IconButton, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Search, SearchIconWrapper, StyledInputBase } from "./Header.style";
+
+const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
+          >
+            TRIP ADVISOR APP
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
